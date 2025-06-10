cca5d7fdaab0c2b23f82d0f13eae32a022daabc0
fardeenhimself <fardeenviper77@gmail.com>
2025-06-10 22:40:54 +0600

    UI Updated

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFFd3ffce);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -20,19 +24,41 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child: Row(
                   children: [
-                    Expanded(child: CustomCard(colour: Colors.green[100])),
-                    Expanded(child: CustomCard(colour: Colors.green[100])),
+                    Expanded(
+                      child: CustomCard(
+                        cardChild: GenderCard(
+                          icon: FontAwesomeIcons.mars,
+                          text: 'MALE',
+                        ),
+                        colour: activeCardColour,
+                      ),
+                    ),
+                    Expanded(
+                      child: CustomCard(
+                        cardChild: GenderCard(
+                          icon: FontAwesomeIcons.venus,
+                          text: 'FEMALE',
+                        ),
+                        colour: activeCardColour,
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Expanded(child: CustomCard(colour: Colors.green[100])),
+              Expanded(child: CustomCard(colour: activeCardColour)),
               Expanded(
                 child: Row(
                   children: [
-                    Expanded(child: CustomCard(colour: Colors.green[100])),
-                    Expanded(child: CustomCard(colour: Colors.green[100])),
+                    Expanded(child: CustomCard(colour: activeCardColour)),
+                    Expanded(child: CustomCard(colour: activeCardColour)),
                   ],
                 ),
+              ),
+              Container(
+                color: Color(0xFF91f086),
+                margin: EdgeInsets.only(top: 10.0),
+                width: double.infinity,
+                height: bottomContainerHeight,
               ),
             ],
           ),
@@ -42,10 +68,32 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class GenderCard extends StatelessWidget {
+  const GenderCard({required this.icon, required this.text});
+
+  final IconData? icon;
+  final String? text;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: Colors.green[300], size: 80.0),
+        SizedBox(height: 10.0),
+        Text(
+          '$text',
+          style: TextStyle(fontSize: 20.0, color: Colors.green[300]),
+        ),
+      ],
+    );
+  }
+}
+
 class CustomCard extends StatelessWidget {
-  const CustomCard({required this.colour});
+  const CustomCard({required this.colour, this.cardChild});
 
   final Color? colour;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +103,7 @@ class CustomCard extends StatelessWidget {
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
     );
   }
 }
